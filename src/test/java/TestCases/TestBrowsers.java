2cb8776de528c1d8ae78771e1cb6d646ac93513d
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-01-08 17:56:42 +0600

    office work is done

--- src/test/java/TestCases/TestBrowsers.java
+++ src/test/java/TestCases/TestBrowsers.java
@@ -1,13 +1,20 @@
 package TestCases;
 
+import net.bytebuddy.implementation.FieldAccessor;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.manager.SeleniumManager;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.util.logging.Logger;
+
 public class TestBrowsers {
+
+
+    public static Logger log = Logger.getLogger(TestBrowsers.class.getName());
     static ChromeDriver driver = new ChromeDriver();
     static FirefoxDriver ffDriver = new FirefoxDriver();
+    FieldAccessor.PropertyConfigurable.Co
 
     public void ChromeDriver () {
         driver.get("https://www.google.com");
@@ -19,10 +26,11 @@ public class TestBrowsers {
     public static void main(String[] args) {
         TestBrowsers test = new TestBrowsers();
         test.ChromeDriver();
+        log.info(driver.getCurrentUrl());
         test.firefoxDriver();
+        log.info("Driver closed");
+
         driver.close();
         ffDriver.close();
-
-
     }
 }
