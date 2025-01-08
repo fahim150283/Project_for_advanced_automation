da610cb2c9063efd86924c07b35a5e861ee9b8a6
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-01-08 17:09:07 +0600

    this is the 2nd commit

--- src/test/java/TestCases/TestBrowsers.java
+++ src/test/java/TestCases/TestBrowsers.java
@@ -1,4 +1,28 @@
 package TestCases;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.manager.SeleniumManager;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class TestBrowsers {
+    static ChromeDriver driver = new ChromeDriver();
+    static FirefoxDriver ffDriver = new FirefoxDriver();
+
+    public void ChromeDriver () {
+        driver.get("https://www.google.com");
+    }
+    public void firefoxDriver () {
+        ffDriver.get("https://www.google.com");
+    }
+
+    public static void main(String[] args) {
+        TestBrowsers test = new TestBrowsers();
+        test.ChromeDriver();
+        test.firefoxDriver();
+        driver.close();
+        ffDriver.close();
+
+
+    }
 }
