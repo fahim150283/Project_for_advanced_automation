34955f0959e9669ee4755cbfeb129c7800ba113b
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-01-09 17:22:17 +0600

    awesome

--- src/main/java/TestLog4j.java
+++
@@ -1,7 +0,0 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class TestLog4j {
-    // Creating a logger
-    private static Logger logger = LogManager.getLogger();
-}
--- src/test/java/TestCases/TestBrowsers.java
+++ src/test/java/TestCases/TestBrowsers.java
@@ -4,31 +4,42 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class TestBrowsers {
 
 
-    public static Logger log = LogManager.getLogger();
-    static ChromeDriver driver = new ChromeDriver();
-    static FirefoxDriver ffDriver = new FirefoxDriver();
+    public static Logger log = LogManager.getLogger(TestBrowsers.class.getName());
+    static ChromeDriver driver;
+    static FirefoxDriver ffDriver;
 
-    public void ChromeDriver () {
+    @BeforeTest
+    public void setUp() {
+        driver= new ChromeDriver();
         driver.manage().window().maximize();
+//        ffDriver = new FirefoxDriver();
+//        ffdriver.manage().window().maximize();
+    }
+
+    @Test
+    public static void test123() {
+
         driver.get("https://www.google.com");
-    }
-    public void firefoxDriver () {
-        ffDriver.manage().window().maximize();
-        ffDriver.get("https://www.google.com");
-    }
-
-    public static void main(String[] args) {
-        TestBrowsers test = new TestBrowsers();
-        test.ChromeDriver();
         log.info("Logger: {}", driver.getCurrentUrl());
-        test.firefoxDriver();
-        log.info("Logger: Driver closed");
 
-        driver.close();
-        ffDriver.close();
+        //get title
+        String title = driver.getTitle();
+
+        System.out.println(title);
+
+
+    }
+
+    @AfterTest
+    public static void afterTest() {
+        driver.close();     //close current browser
+        driver.quit();      //close all the browsers
     }
 }
