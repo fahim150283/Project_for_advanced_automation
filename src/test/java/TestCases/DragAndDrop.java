93168b639dfb26e7fc5271e1a6e425666f63b143
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-02-09 12:35:05 +0600

    drag and drop is complete

---
+++ src/test/java/TestCases/DragAndDrop.java
@@ -0,0 +1,41 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+public class DragAndDrop extends TestBrowsers {
+
+    @Test
+    public static void dragAndDrop() throws InterruptedException {
+        fluentwait.until(
+                d -> {
+                    try {
+                        // navigate to google
+                        driver.get("https://jqueryui.com/droppable/");
+                        log.info("Navigated to: " + driver.getCurrentUrl());
+
+                        List<WebElement> frame1 = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.tagName("iframe")));
+                        WebElement frame = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("demo-frame")));
+                        driver.switchTo().frame(frame);
+                        WebElement draggable = driver.findElement(By.id("draggable"));
+                        WebElement droppable = driver.findElement(By.id("droppable"));
+                        wait.until(ExpectedConditions.elementToBeClickable(draggable));
+                        wait.until(ExpectedConditions.elementToBeClickable(droppable));
+
+                        Actions action = new Actions(driver);
+                        action.dragAndDrop(draggable, droppable).perform();
+
+                        Thread.sleep(2000);
+
+                    } catch (NoAlertPresentException | InterruptedException e) {
+                    }
+                    return true;
+                });
+    }
+}
