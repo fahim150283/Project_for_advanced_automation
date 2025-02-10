cbcce3e63d82a7aba0ae80ddd7840b8fee5569d8
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-02-10 17:12:48 +0600

    the TestMOCGeoLocation is complete

---
+++ src/test/java/TestCases/TestMOCGeoLocation.java
@@ -0,0 +1,28 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v127.emulation.Emulation;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+public class TestMOCGeoLocation extends TestBrowsers{
+
+    @Test
+    public static void TestMOCGeoLocation() throws InterruptedException {
+        DevTools devTools = ((ChromeDriver) driver).getDevTools();
+        devTools.createSession();
+
+
+        devTools.send(Emulation.setGeolocationOverride(Optional.of(21.470754377411506), Optional.of(-158.2180186190138), Optional.of(100)));
+
+        driver.get("https://www.gps-coordinates.net/my-location");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"addr\"]")));
+        System.out.println(driver.findElement(By.xpath("//*[@id=\"wrap\"]/div[2]/div[2]/div[2]/p")).getText());
+    }
+}
