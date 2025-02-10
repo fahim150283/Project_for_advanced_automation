fef342b814a49c7c0ec89ed61cb2547b6e1252a6
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-02-10 12:47:20 +0600

    the tabs and popups are done

---
+++ src/test/java/TestCases/TestTabsAndPopUps.java
@@ -0,0 +1,54 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class TestTabsAndPopUps extends TestBrowsers {
+
+    @Test
+    public void testTabsAndPopUps() throws InterruptedException {
+        fluentwait.until(
+                d -> {
+                    driver.get("https://hdfcbank.com/");
+                    log.info("Navigated to: " + driver.getCurrentUrl());
+                    Actions action = new Actions(driver);
+                    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"custom-button\"]/div/div/div/button")));
+                    action.moveToElement(driver.findElement(By.xpath("//*[@id=\"custom-button\"]/div/div/div/button"))).click().perform();
+
+                    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"main\"]/div[2]/div[11]/div[3]/div/div/div/div/div/div[1]/div/div[1]/a")));
+                    driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div[11]/div[3]/div/div/div/div/div/div[1]/div/div[1]/a")).click();
+
+                    Set<String> windows = driver.getWindowHandles();
+                    Iterator<String> iterator = windows.iterator();
+
+                    String [] windowArray = new String[windows.size()];
+                    for (int i = 0; i < windows.size(); i++) {
+                        windowArray[i] = iterator.next();
+                    }
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    driver.switchTo().window(windowArray[0]);
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    driver.switchTo().window(windowArray[1]);
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return true;
+                });
+
+    }
+}
