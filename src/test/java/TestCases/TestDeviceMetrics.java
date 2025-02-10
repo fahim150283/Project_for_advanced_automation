87133384990ecb548d828d730c869c39fa6be228
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-02-10 17:40:10 +0600

    the TestDeviceMetrics is done

---
+++ src/test/java/TestCases/TestDeviceMetrics.java
@@ -0,0 +1,35 @@
+package TestCases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v128.emulation.Emulation;
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+public class TestDeviceMetrics extends TestBrowsers{
+
+    @Test
+    public static void TestMOCGeoLocation() throws InterruptedException {
+        DevTools devTools = ((ChromeDriver)driver).getDevTools();
+        devTools.createSession();
+        devTools.send(Emulation.setDeviceMetricsOverride(
+                480,  // Width
+                540,  // Height
+                50,   // Scale (Default: 100%)
+                true,  // Mobile mode enabled
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty()
+        ));
+
+        driver.get("https://youtube.com");
+    }
+}
