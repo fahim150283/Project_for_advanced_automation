aedf488f9ad2248553af6f92d7ce27b1c78cdf7c
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-02-10 16:47:01 +0600

    the TestBadSSL is done:

---
+++ src/test/java/TestCases/TestBadSSL.java
@@ -0,0 +1,27 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v127.security.Security;
+import org.testng.annotations.Test;
+
+public class TestBadSSL extends TestBrowsers{
+
+    @Test
+    public static void TestBadSSL() throws InterruptedException {
+        DevTools devTools = ((ChromeDriver) driver).getDevTools();
+        devTools.createSession();
+
+
+        devTools.send(Security.enable());
+        //This enables the page to be visible even if the page can not be seen followed by this error "Your connection is not private"
+        devTools.send(Security.setIgnoreCertificateErrors(true));
+
+
+        driver.get("https://expired.badssl.com");
+        Thread.sleep(2000);
+        xpath = "//*[@id=\"content\"]/h1";
+        System.out.println(driver.findElement(By.xpath(xpath)).getText());
+    }
+}
---
+++ src/test/java/TestCases/TestShadowDOM.java
@@ -0,0 +1,33 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class TestShadowDOM extends TestBrowsers{
+
+    @Test
+    public static void testShadowDOM(){
+        driver.get("chrome://downloads/");
+
+        WebElement root = driver.findElement(By.cssSelector("downloads-manager"));
+
+        WebElement shadowRoot1 = (WebElement) ((JavascriptExecutor)driver).executeScript("return arguments[0].shadowRoot",root);
+
+        assert shadowRoot1 != null;
+        WebElement root2 = shadowRoot1.findElement(By.cssSelector("downloads-toolbar"));
+        WebElement shadowRoot2 = (WebElement) ((JavascriptExecutor)driver).executeScript("return arguments[0].shadowRoot",root2);
+
+        assert shadowRoot2 != null;
+        WebElement root3 = shadowRoot2.findElement(By.cssSelector("cr-toolbar"));
+        WebElement shadowRoot3 = (WebElement) ((JavascriptExecutor)driver).executeScript("return arguments[0].shadowRoot",root3);
+
+
+        assert shadowRoot3 != null;
+        WebElement root4 = shadowRoot3.findElement(By.cssSelector("cr-toolbar-search-field"));
+        WebElement shadowRoot4 = (WebElement) ((JavascriptExecutor)driver).executeScript("return arguments[0].shadowRoot",root4);
+
+        shadowRoot4.findElement(By.cssSelector("#searchInput")).sendKeys("Hello");
+    }
+}
