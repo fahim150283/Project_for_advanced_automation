6f669537b71c0f1c2db5256c1bef9fcd5f67965f
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-02-11 12:20:58 +0600

    test time zone is complete

---
+++ src/test/java/TestCases/TestOverrideTimeZone.java
@@ -0,0 +1,35 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v129.emulation.Emulation;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class TestOverrideTimeZone extends TestBrowsers {
+
+    @Test
+    public static void overrideTimeZone() {
+        fluentwait.until(d -> {
+            try {
+                driver.get("https://webbrowsertools.com/timezone/");
+                WebElement pretimezone = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"timeZone\"]")));
+                System.out.println("pretimezone: " + pretimezone.getText());
+                Thread.sleep(5000);
+
+                DevTools devTools = ((ChromeDriver) driver).getDevTools();
+                devTools.createSession();
+                devTools.send(Emulation.setTimezoneOverride("EST"));
+
+                driver.get("https://webbrowsertools.com/timezone/");
+                WebElement posttimezone = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"timeZone\"]")));
+                System.out.println("posttimezone: " + posttimezone.getText());
+            } catch (
+                    NoAlertPresentException | InterruptedException e) {}
+            return true;
+        });
+    }
+}
