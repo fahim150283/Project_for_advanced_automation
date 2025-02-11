c46ae31c4a7e5d28364540fbb289dc83bf473840
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-02-11 11:37:35 +0600

    the TestNetworkSpeed is done

---
+++ src/test/java/TestCases/TestBlockingNetworkRequest.java
@@ -0,0 +1,35 @@
+package TestCases;
+
+import com.google.common.collect.ImmutableList;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v129.network.Network;
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+public class TestBlockingNetworkRequest extends TestBrowsers{
+
+    @Test
+    public static void BlockingNetworkRequest() throws InterruptedException {
+        fluentwait.until(
+                d -> {
+                    try {
+                        DevTools devTools = ((ChromeDriver) driver).getDevTools();
+                        devTools.createSession();
+                        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+                        devTools.send(Network.setBlockedURLs(ImmutableList.of("*.jpg","*.png","*.jpeg")));
+
+                        // navigate to google
+                        driver.get("https://onepagelove.com/tag/big-images");
+                        log.info("Navigated to: " + driver.getCurrentUrl());
+
+                        Thread.sleep(5000);
+                    } catch (NoAlertPresentException | InterruptedException e) {
+                    }
+                    return true;
+                }
+        );
+    }
+}
---
+++ src/test/java/TestCases/TestNetworkSpeed.java
@@ -0,0 +1,43 @@
+package TestCases;
+
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v129.network.Network;
+import org.openqa.selenium.devtools.v129.network.model.ConnectionType;
+import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+public class TestNetworkSpeed extends TestBrowsers {
+
+    @Test
+    public static void networkSpeed() {
+        fluentwait.until(
+                d -> {
+                    try {
+                        DevTools devTools = ((ChromeDriver) driver).getDevTools();
+                        devTools.createSession();
+
+
+                        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+                        devTools.send(Network.emulateNetworkConditions(
+                                false,
+                                1000,   //Latency in miliseconds
+                                200000, // Download throughput (200,000 bps or 200 Kbps)
+                                100000, // Upload throughput (100,000 bps or 100 Kbps)
+                                Optional.of(ConnectionType.ETHERNET),
+                                Optional.empty(),
+                                Optional.empty(),
+                                Optional.empty()));
+
+                        // navigate to google
+                        driver.get("https://onepagelove.com/tag/big-images");
+                        log.info("Navigated to: " + driver.getCurrentUrl());
+                    } catch (NoAlertPresentException e) {
+                    }
+                    return true;
+                }
+        );
+    }
+}
