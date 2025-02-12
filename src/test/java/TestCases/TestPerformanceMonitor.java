50d25268dd2a658dba3ceee0f79abe21cb433704
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-02-12 11:25:36 +0600

    the file is updated for TestPerfomanceMonitor

---
+++ src/test/java/TestCases/TestPerformanceMonitor.java
@@ -0,0 +1,34 @@
+package TestCases;
+
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v127.performance.Performance;
+import org.openqa.selenium.devtools.v127.performance.model.Metric;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TestPerformanceMonitor extends TestBrowsers {
+
+    @Test
+    public void testPerformanceMonitor() throws InterruptedException {
+        fluentwait.until(
+                d -> {
+                    try {
+                        DevTools devTools = ((ChromeDriver) driver).getDevTools();
+                        devTools.createSession();
+                        devTools.send(Performance.enable(Optional.of(Performance.EnableTimeDomain.TIMETICKS)));
+                        driver.get("https://gb-storefront-8r4z.vercel.app/");
+                        Thread.sleep(5000);
+
+                        List<Metric> metrics = devTools.send(Performance.getMetrics());
+
+                        metrics.forEach(metric -> System.out.println("Metric name is : " + metric.getName() + " and the Value is : " + metric.getValue()));
+                    } catch (NoAlertPresentException | InterruptedException e) {
+                    }
+                    return true;
+                });
+    }
+}
--- src/test/java/TestCases/TestRequestAndResposneHeaders.java
+++ src/test/java/TestCases/TestRequestAndResposneHeaders.java
@@ -100,40 +100,24 @@ public class TestRequestAndResposneHeaders extends TestBrowsers {
                 devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
 
                 devTools.addListener(Network.requestWillBeSent(), request -> {
-
                     Headers header = request.getRequest().getHeaders();
-
                     if (!header.isEmpty()) {
-
                         System.out.println("********************                 Request Headers                 ********************");
                         header.forEach((key, value) -> {
-
                             System.out.println("  " + key + " = " + value);
-
                         });
-
                     }
-
                 });
 
                 devTools.addListener(Network.responseReceived(), response -> {
-
                     Headers header = response.getResponse().getHeaders();
-
                     if (!header.isEmpty()) {
-
                         System.out.println("********************                 Response Headers                 ********************");
                         header.forEach((key, value) -> {
-
                             System.out.println("  " + key + " = " + value);
-
                         });
-
                     }
-
                     System.out.println("Response URL is : "+response.getResponse().getUrl()+"  status code is : "+response.getResponse().getStatus());
-
-
                 });
 
                 Map<String, Object> headers = new HashMap<String,Object>();
