0aab21e867dc15ad6cbce78871a25bda0092f73f
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-02-16 13:09:35 +0600

    the multiple data provider is complete

--- src/test/java/ExcelReader/DataProviders.java
+++ src/test/java/ExcelReader/DataProviders.java
@@ -3,6 +3,8 @@ package ExcelReader;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
+
 public class DataProviders {
 
     @Test(dataProvider = "dp1")
@@ -18,6 +20,46 @@ public class DataProviders {
 
         data[1][0] = "Jane";
         data[1][1] = "Hoe";
+
+        return data;
+    }
+
+    @DataProvider(name = "dp2")
+    public Object[][] getData2() {
+        Object[][] data = new Object[2][3];
+        data[0][0] = "John";
+        data[0][1] = "Doe";
+        data[0][2] = "john.doe@example.com";
+
+        data[1][0] = "Jane";
+        data[1][1] = "Hoe";
+        data[1][2] = "jane.hoe@example.com";
+
+        return data;
+    }
+
+
+    @DataProvider(name = "dp3")
+    public Object[][] DataForMethodTest(Method method) {
+
+        Object[][] data =null;
+        if (method.getName().equals("testLogin")) {
+            data = new Object[2][2];
+            data[0][0] = "John";
+            data[0][1] = "Doe";
+            data[1][0] = "Jane";
+            data[1][1] = "Hoe";
+        } else if (method.getName().equals("testUserReg")) {
+            data = new Object[2][3];
+            data[0][0] = "John";
+            data[0][1] = "Doe";
+            data[0][2] = "john.doe@example.com";
+
+            data[1][0] = "Jane";
+            data[1][1] = "Hoe";
+            data[1][2] = "jane.hoe@example.com";
+        }
+
 
         return data;
     }
--- src/test/java/TestCases/NewTestForDataProvider.java
+++ src/test/java/TestCases/NewTestForDataProvider.java
@@ -1,4 +1,17 @@
 package TestCases;
 
+import ExcelReader.DataProviders;
+import org.testng.annotations.Test;
+
 public class NewTestForDataProvider {
+
+    @Test(dataProviderClass = DataProviders.class, dataProvider = "dp3")
+    public void testLogin(String firstName, String lastName) {
+        System.out.println(firstName + " " + lastName);
+    }
+
+    @Test(dataProviderClass = DataProviders.class, dataProvider = "dp3")
+    public void testUserReg(String firstName, String lastName, String email) {
+        System.out.println(firstName + " " + lastName + " " + email);
+    }
 }
