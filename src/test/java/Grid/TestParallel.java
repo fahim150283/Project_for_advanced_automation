945823f53e86c8697ac8ffae002554ecfed1abdf
Abul Hossain Chowdhury <fahim150283@gmail.com>
2025-04-22 15:38:59 +0600

    the selenium grid looks to be just fine and completed

--- src/test/java/Grid/TestParallel.java
+++ src/test/java/Grid/TestParallel.java
@@ -1,43 +1,59 @@
 package Grid;
 
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.safari.SafariOptions;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class TestParallel {
 
+    SoftAssert softAssert = new SoftAssert();
     protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<RemoteWebDriver>();
 
     public WebDriver getDriver() {
         return driver.get();
     }
 
-    @Parameters({"browser", "url"})
+    @Parameters({"browser", "headless", "url"})
     @Test
-    public void doSearch(String browser, String url) throws MalformedURLException {
-        Capabilities opt = null; // Declare the opt variable
-        if (browser.equals("firefox")) {
-            opt = new FirefoxOptions();
-        } else if (browser.equals("chrome")) {
-            opt = new ChromeOptions();
-//            opt.addArguments("--no-sandbox");
-//            opt.addArguments("--disable-dev-shm-usage");
-//            opt.addArguments("--remote-allow-origins=*");
-//            opt.addArguments("--user-data-dir=/tmp/unique-profile-" + System.currentTimeMillis());
-        } else if (browser.equals("edge")) {
-            opt = new EdgeOptions();
+    public void doSearch(String browser, String headless, String url) throws MalformedURLException {
+        RemoteWebDriver dr = null;
+        try {
+            if (browser.equals("firefox")) {
+                FirefoxOptions opt = new FirefoxOptions();
+                dr = new RemoteWebDriver(new URL(url), opt);
+            } else if (browser.equals("chrome")) {
+                ChromeOptions opt = new ChromeOptions();
+            opt.addArguments("--no-sandbox");
+            opt.addArguments("--disable-dev-shm-usage");
+            opt.addArguments("--remote-allow-origins=*");
+            opt.addArguments("--user-data-dir=/tmp/unique-profile-" + System.currentTimeMillis());
+                dr = new RemoteWebDriver(new URL(url), opt);
+            } else if (browser.equals("edge")) {
+                EdgeOptions opt = new EdgeOptions();
+                opt.addArguments("--no-sandbox");
+                opt.addArguments("--disable-dev-shm-usage");
+                opt.addArguments("--remote-allow-origins=*");
+                opt.addArguments("--user-data-dir=/tmp/unique-profile-" + System.currentTimeMillis());
+                dr = new RemoteWebDriver(new URL(url), opt);
+            }
+
+            driver.set(dr);
+            System.out.println(browser + " opened");
+            softAssert.assertTrue(driver.get().getCurrentUrl().contains("google"));
+
+            Thread.sleep(50000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        driver.set(new RemoteWebDriver(new URL(url), opt)); // No need to cast opt to Capabilities
-        System.out.println(browser + " opened");
+        driver.get().quit();
+        driver.remove();
     }
 }
